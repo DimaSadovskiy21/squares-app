114a4bd2f3be78c5bade71d3beb1241a05c5ce3e
DimaSadovsky21 <dimasadovsky21@gmail.com>
2022-12-30 16:10:23 +0300

    final commit

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,10 +1,23 @@
-import { View } from "react-native"
-import { styles } from "./Styles"
+import { View } from 'react-native';
+import { useAppDispatch } from '../hooks/redux';
+import { SquareType } from '../models/Square';
+import { deleteColor } from '../store/colorSlice';
+import { styles } from './Styles';
+import { SuperButton } from './SuperButton';
 
 type SquarePropsType = {
-    rgbColor: string
-}
+  square: SquareType;
+};
 
-export const Square:React.FC<SquarePropsType> = ({rgbColor}) => {
-    return <View style={[styles.square, { backgroundColor: rgbColor }]} />
-}
+export const Square: React.FC<SquarePropsType> = ({ square }) => {
+  const dispatch = useAppDispatch();
+
+  const onPressDeleteSquareHandler = (square: SquareType) => {
+    dispatch(deleteColor(square));
+  };
+  return (
+    <SuperButton  pressHandler={() => onPressDeleteSquareHandler(square)}>
+      <View style={[styles.square, { backgroundColor: square.rgbColor }]} />
+    </SuperButton>
+  );
+};
--- src/components/Squares.tsx
+++ src/components/Squares.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
-import { useAppDispatch, useAppSelector } from '../hooks/redux';
-import { SquareType } from '../models/Square';
-import { deleteColor } from '../store/colorSlice';
+import { useAppSelector } from '../hooks/redux';
 import { Square } from './Square';
 import { styles } from './Styles';
-import { SuperButton } from './SuperButton';
+
 
 export const Squares = () => {
   const squares = useAppSelector((state) => state.colorSlice.squares);
-  const dispatch = useAppDispatch();
-
-  const onPressDeleteSquareHandler = (square: SquareType) => {
-    dispatch(deleteColor(square));
-  };
 
   const squaresView = squares.map((item) => {
-    return (
-      <SuperButton key={item.id} pressHandler={() => onPressDeleteSquareHandler(item)}>
-        <Square rgbColor={item.rgbColor} />
-      </SuperButton>
-    );
+    return <Square key={item.id} square={item} />;
   });
 
   return <View style={styles.squares}>{squaresView}</View>;

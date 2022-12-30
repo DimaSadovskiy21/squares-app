9cdbcd6166c2feae25eab4de145fa357ca865ef5
DimaSadovsky21 <dimasadovsky21@gmail.com>
2022-12-30 16:12:49 +0300

    final commit

--- src/components/Squares.tsx
+++ src/components/Squares.tsx
@@ -4,13 +4,10 @@ import { useAppSelector } from '../hooks/redux';
 import { Square } from './Square';
 import { styles } from './Styles';
 
-
 export const Squares = () => {
   const squares = useAppSelector((state) => state.colorSlice.squares);
 
-  const squaresView = squares.map((item) => {
-    return <Square key={item.id} square={item} />;
-  });
+  const squaresView = squares.map((item) => <Square key={item.id} square={item} />);
 
   return <View style={styles.squares}>{squaresView}</View>;
 };
